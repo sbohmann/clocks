7ce07134a2731d4e055efd7d32d2334065bf0433
sbohmann <github@yeoman.at>
2023-09-27 22:04:21 +0200

    terminal clock

--- terminal-clock/clock.c
+++ terminal-clock/clock.c
@@ -1,6 +1,8 @@
 #include <curses.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <inttypes.h>
+
 
 int main()
 {
@@ -8,7 +10,7 @@ int main()
     cbreak();
     noecho();
 
-    for (int x = 0; x < COLS; ++x) {
+    while (1) {
         struct timeval timeval;
         struct timezone timezone;
         gettimeofday(&timeval, &timezone);
@@ -18,15 +20,22 @@ int main()
         int tz_minuteswest = timezone.tz_minuteswest;
         int tz_dsttime = timezone.tz_dsttime;
 
+        int64_t whole_second_of_day = tv_sec % 86400;
+        int64_t second_of_day = whole_second_of_day * 1000 * 1000 + tv_usec;
+        int64_t second_of_minute = second_of_day % (60 * 1000 * 1000);
+
+        int64_t secondPosition = second_of_minute * COLS / 1000 / 1000 / 60;
+
+        char buffer[2000];
+        sprintf(buffer, "second_of_day: %" PRId64 ", secondPosition: %" PRId64 "\n", second_of_day, secondPosition);
+
         clear();
-        mvaddch(0, x, '+');
+        mvaddch(0, secondPosition, 'S');
         mvaddch(LINES - 1, 0, '-');
-        mvaddstr(10, 30, "press any key to quit");
+        mvaddstr(10, 30, buffer);
         refresh();
         usleep(100000);
     }
-
-    getch();
 
     endwin();
 }
