281b30b303af3bb3099bdac82f8f3abc2eaaaaee
sbohmann <github@yeoman.at>
2023-10-15 13:43:55 +0200

    Clock PWA

--- html-clock/clock.js
+++ html-clock/clock.js
@@ -1,23 +1,30 @@
 const secondsPerDay = 60 * 60 * 24
 const millisecondsPerDay = 1000 * secondsPerDay
 
-const SHORT_TICK_START_RADIUS = 0.42
-const LONG_TICK_START_RADIUS = 0.39
-const TICK_END_RADIUS = 0.45
-const SECOND_HANDLE_START_RADIUS = -0.07
-const SECOND_HANDLE_END_RADIUS = 0.37
-const MINUTE_HANDLE_START_RADIUS = -0.07
-const MINUTE_HANDLE_END_RADIUS = 0.37
-const HOUR_HANDLE_START_RADIUS = -0.07
+const SHORT_TICK_START_RADIUS = 0.405
+const LONG_TICK_START_RADIUS = 0.349
+const TICK_END_RADIUS = 0.4618
+const SECOND_HANDLE_START_RADIUS = -0.06
+const SECOND_HANDLE_END_RADIUS = 0.375
+const MINUTE_HANDLE_START_RADIUS = -0.06
+const MINUTE_HANDLE_END_RADIUS = 0.375
+const HOUR_HANDLE_START_RADIUS = -0.06
 const HOUR_HANDLE_END_RADIUS = 0.27
 
 class Clock extends HTMLElement {
-    #canvas;
+    #faceCanvas;
+    #handsCanvas;
     #animationFrameRequested = false;
+    #lastWidth
+    #lastHeight
 
     connectedCallback() {
-        this.#canvas = document.createElement('canvas')
-        this.appendChild(this.#canvas)
+        this.#faceCanvas = document.createElement('canvas')
+        this.#faceCanvas.id = 'face-canvas'
+        this.appendChild(this.#faceCanvas)
+        this.#handsCanvas = document.createElement('canvas')
+        this.#handsCanvas.id = 'hands-canvas'
+        this.appendChild(this.#handsCanvas)
         this.requestRefresh()
         window.onresize = () => this.requestRefresh()
     }
@@ -32,20 +39,28 @@ class Clock extends HTMLElement {
 
     refreshCanvas() {
         this.#animationFrameRequested = false
-        let width = this.#canvas.offsetWidth * window.devicePixelRatio
-        let height = this.#canvas.offsetHeight * window.devicePixelRatio
-        if (this.#canvas.width !== width || this.#canvas.height !== height) {
-            this.#canvas.width = width
-            this.#canvas.height = height
+        let width = this.#handsCanvas.offsetWidth * window.devicePixelRatio
+        let height = this.#handsCanvas.offsetHeight * window.devicePixelRatio
+        if (this.#handsCanvas.width !== width || this.#handsCanvas.height !== height) {
+            this.#handsCanvas.width = width
+            this.#handsCanvas.height = height
+        }
+        if (this.#faceCanvas.width !== width || this.#faceCanvas.height !== height) {
+            this.#faceCanvas.width = width
+            this.#faceCanvas.height = height
         }
         this.drawClock()
         this.requestRefresh()
     }
 
     drawClock() {
-        let context = this.#canvas.getContext('2d')
-        let width = this.#canvas.width
-        let height = this.#canvas.height
+        let faceContext = this.#faceCanvas.getContext('2d')
+        let handsContext = this.#handsCanvas.getContext('2d')
+        let width = this.#handsCanvas.width
+        let height = this.#handsCanvas.height
+        let sameSize = (width === this.#lastWidth && height === this.#lastHeight)
+        this.#lastWidth = width
+        this.#lastHeight = height
         let size = Math.min(width, height)
         let centerX = width / 2
         let centerY = height / 2
@@ -55,31 +70,47 @@ class Clock extends HTMLElement {
             currentDate.getTimezoneOffset() * 60_000
         let millisecondOfDay = localTimestamp % millisecondsPerDay
 
-        context.clearRect(0, 0, width, height)
-        drawRim()
+        if (!sameSize) {
+            drawFace()
+        }
+
+        handsContext.clearRect(0, 0, width, height)
         drawHourHand()
         drawMinuteHand()
         drawSecondHand()
 
-        function drawRim() {
-            context.strokeStyle = 'black'
-            context.lineWidth = window.devicePixelRatio
+        function drawFace() {
+            faceContext.clearRect(0, 0, width, height)
+            faceContext.strokeStyle = 'black'
+            faceContext.lineWidth = size / 2700 * 2
+            faceContext.fillStyle = 'white'
+            faceContext.beginPath()
+            faceContext.ellipse(width / 2, height / 2, size / 2 * 0.98, size / 2 * 0.98, 0, 0, 360)
+            faceContext.stroke()
+            faceContext.fill()
             let numberOfTicks = 5 * 12;
             for (let n = 0; n < numberOfTicks; ++n) {
                 let tickIsLong = (n % 5 === 0)
+                faceContext.strokeStyle = tickIsLong
+                    ? '#DBBA73'
+                    : '#1A1266'
+                faceContext.lineWidth = tickIsLong
+                    ? size / 2700 * 42
+                    : size / 2700 * 27
                 drawRadialLineForPartOfCircle(
                     n / numberOfTicks,
                     tickIsLong
                         ? LONG_TICK_START_RADIUS
                         : SHORT_TICK_START_RADIUS,
-                    TICK_END_RADIUS)
+                    TICK_END_RADIUS,
+                    faceContext)
             }
         }
 
         function drawHourHand() {
             let hourOfHalfDay = millisecondOfDay / 1000 / 60 / 60 / 12
-            context.strokeStyle = 'black'
-            context.lineWidth = 3 * window.devicePixelRatio
+            handsContext.strokeStyle = '#1A1266'
+            handsContext.lineWidth = size / 2700 * 27
             drawRadialLineForPartOfCircle(
                 hourOfHalfDay % 12,
                 HOUR_HANDLE_START_RADIUS,
@@ -88,8 +119,8 @@ class Clock extends HTMLElement {
 
         function drawMinuteHand() {
             let minuteOfHour = millisecondOfDay / 1000 / 60 / 60
-            context.strokeStyle = 'black'
-            context.lineWidth = 3 * window.devicePixelRatio
+            handsContext.strokeStyle = '#1A1266'
+            handsContext.lineWidth = size / 2700 * 27
             drawRadialLineForPartOfCircle(
                 minuteOfHour % 60,
                 MINUTE_HANDLE_START_RADIUS,
@@ -98,15 +129,15 @@ class Clock extends HTMLElement {
 
         function drawSecondHand() {
             let secondOfMinute = millisecondOfDay / 1000 / 60
-            context.strokeStyle = 'red'
-            context.lineWidth = window.devicePixelRatio
+            handsContext.strokeStyle = '#D30449'
+            handsContext.lineWidth = size / 2700 * 15
             drawRadialLineForPartOfCircle(
                 secondOfMinute % 60,
                 SECOND_HANDLE_START_RADIUS,
                 SECOND_HANDLE_END_RADIUS)
         }
 
-        function drawRadialLineForPartOfCircle(partOfCircle, startRadius, endRadius) {
+        function drawRadialLineForPartOfCircle(partOfCircle, startRadius, endRadius, context = handsContext) {
             let angle = angleForPartOfCircle(partOfCircle)
             // normalized direction
             let [nx, ny] = directionForAngle(angle)
